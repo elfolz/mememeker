74f249a4ed908290d14fa57df6ed03b3b8cdbd13
Luiz <lhvalentim@outlook.coom>
2022-06-29 19:35:47 -0300

    sw cache

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,24 @@
-self.addEventListener('install', function (event) {
+const assets = [
+	'/',
+	'script.js',
+	'style.css',
+	'favicon.ico',
+	'image/copy.svg',
+	'image/image.svg',
+	'image/download.svg',
+	'image/clipboard.svg',
+	'android-chrome-192x192.png',
+	'android-chrome-512x512.png',
+	'android-chrome-maskable-192x192.png',
+	'android-chrome-maskable-512x512.png'
+]
+
+self.addEventListener('install', event => {
+	event.waitUntil(() => {
+		caches.open('mememeker')
+		.then(response => {
+			response.addAll(assets)
+		})
+	})
 	self.skipWaiting()
 })
